a606f9c22686b82bf5f430cd3125c7c69dea9147
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-06 23:38:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,24 +19,23 @@ export default function Home() {
       <div className="max-w-4xl mx-auto">
 
 <>
-  {/* TITOLO IN ALTO - COMPLETAMENTE A SINISTRA, SOTTILE, NO SFONDO */}
+  {/* TITOLO IN ALTO A SINISTRA - Allineato a sinistra, Font Serif, Nero */}
   <h1 
     ref={titleRef} 
-    className="sticky top-0 z-50 pt-6 pb-2 text-[18px] md:text-[22px] uppercase font-light text-black bg-transparent w-full flex justify-start pl-6 md:pl-10 pointer-events-none tracking-tight"
-    style={{ fontFamily: 'Helvetica, Arial, sans-serif' }}
+    className="sticky top-0 z-50 py-8 text-xl md:text-2xl uppercase tracking-widest font-serif bg-white/90 text-black text-left w-full pl-5 md:pl-10"
+    style={{ fontFamily: 'serif' }}
   >
-    <span className="pointer-events-auto leading-none">DANIELE RAINERI STUDIO</span>
+    <span className="pointer-events-auto">DANIELE RAINERI STUDIO</span>
   </h1>
 
-  {/* TITOLO IN BASSO - STESSA GRANDEZZA, SOTTILE, A DESTRA */}
+  {/* SERIE IN BASSO A DESTRA - Font Serif, Nero */}
   <div 
-    className="fixed bottom-10 right-6 md:right-12 z-50 uppercase text-[18px] md:text-[22px] font-light text-black pointer-events-none tracking-tight"
-    style={{ fontFamily: 'Helvetica, Arial, sans-serif' }}
+    className="fixed bottom-8 right-8 md:right-12 z-50 uppercase text-xs md:text-sm tracking-[0.2em] font-serif text-black pointer-events-none"
+    style={{ fontFamily: 'serif' }}
   >
-    <span className="pointer-events-auto leading-none">SERIES: INTRECCI</span>
+    <span className="pointer-events-auto">SERIES: INTRECCI</span>
   </div>
 </>
-
 
 
 
