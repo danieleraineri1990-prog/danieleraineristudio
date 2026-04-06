3c21bc05e93572a80e7bcb41c054a23446f95c09
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-06 21:56:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,11 @@ export default function Home() {
       <div className="max-w-4xl mx-auto">
        <h1 
   ref={titleRef} 
-  className="sticky top-0 z-50 py-6 md:py-10 text-xs md:text-sm uppercase tracking-[0.3em] md:tracking-[0.4em] font-medium bg-transparent pointer-events-none mix-blend-difference text-white"
+  className="sticky top-0 z-50 py-6 text-xl md:text-2xl uppercase tracking-tight font-bold bg-transparent pointer-events-none mix-blend-difference text-white whitespace-nowrap"
 >
   <span className="pointer-events-auto">DANIELE RAINERI STUDIO</span>
 </h1>
+
 
 
             {/* --- SEZIONE IMMAGINI --- */}
