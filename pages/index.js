070abcdad900e91c4d7a9ec4f24c5ba4cb8837aa
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 11:16:20 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white text-black p-10">
       <Head><title>DANIELE RAINERI STUDIO</title></Head>
+    <meta name="google-site-verification" content="m6HHKQMHvSaU59B2WftQbjoKLC3qFuhA0cJGvdL7j2w" />
       <div className="max-w-4xl mx-auto">
 
 <>
