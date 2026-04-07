3a18454d7a62ed7759c9edd4172777ad22cf50f5
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 09:29:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
     <span className="pointer-events-auto">DANIELE RAINERI STUDIO</span>
   </h1>
 
-  <div className="fixed bottom-10 right-0 z-50 uppercase text-base md:text-xl tracking-[0.2em] font-light mix-blend-difference text-white pointer-events-none">
+  <div className="fixed bottom-4 right-0 z-50 uppercase text-base md:text-xl tracking-[0.2em] font-light mix-blend-difference text-white pointer-events-none">
     <span className="pointer-events-auto">SERIES: INTRECCI</span>
   </div>
 </>
