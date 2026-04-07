779f47d90de81e2e77303efd8c48139961cdc85d
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 17:05:16 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -243,7 +243,7 @@ export default function Home() {
 </div>
       <div className="mt-20 mb-40">
   <img 
-    src="/images/22.JPG" 
+    src="/images/22.jpg" 
     alt="INTRECCIO NO.16" 
     className="w-full h-auto" 
   />
