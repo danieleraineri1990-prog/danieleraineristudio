19c77a4cb4888cf7efc27d3cc6cd9f6b524bc6ac
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 16:25:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -193,6 +193,54 @@ export default function Home() {
     </p>
   </div>
 </div>
+      <div className="mt-20 mb-40">
+  <img 
+    src="/images/25.JPG" 
+    alt="INTRECCIO NO.15" 
+    className="w-full h-auto" 
+  />
+  <div className="mt-4">
+    <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">
+      INTRECCIO NO.15 — Acrylic 120x160
+    </p>
+  </div>
+</div>
+        <div className="mt-20 mb-40">
+  <img 
+    src="/images/20.JPG" 
+    alt="INTRECCIO NO.15" 
+    className="w-full h-auto" 
+  />
+  <div className="mt-4">
+    <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">
+      INTRECCIO NO.13 — Oil 80x120
+    </p>
+  </div>
+</div>
+       <div className="mt-20 mb-40">
+  <img 
+    src="/images/24.JPG" 
+    alt="INTRECCIO NO.34" 
+    className="w-full h-auto" 
+  />
+  <div className="mt-4">
+    <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">
+      INTRECCIO NO.34 — Oil 100x170
+    </p>
+  </div>
+</div>
+      <div className="mt-20 mb-40">
+  <img 
+    src="/images/21.JPG" 
+    alt="INTRECCIO NO.17" 
+    className="w-full h-auto" 
+  />
+  <div className="mt-4">
+    <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">
+      INTRECCIO NO.17 — Oil 80x120
+    </p>
+  </div>
+</div>
 
 
         <div className="mt-12 text-[11px] leading-relaxed max-w-xl text-gray-700 font-light tracking-wide">
