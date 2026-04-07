18dbfb97291a0b839298125c2a422e8170ee7536
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 09:47:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -66,21 +66,21 @@ export default function Home() {
         <div className="mt-20">
           <h2 className="text-[10px] uppercase tracking-widest border-b pb-2 text-gray-700 font-light">Premi</h2>
           <div className="mt-5">
-            <p className="text-[11px] uppercase tracking-widest text-gray-700 font-light">COLLECTIVE EXHIBITION</p>
-            <p className="text-[11px] text-gray-400 mt-2 font-light">Venice pavilion – Biennale The milk of dreams – Vincitore primo premio artefici del nostro tempo</p>
+            <p className="text-[11px] uppercase tracking-widest text-gray-800 font-light">COLLECTIVE EXHIBITION</p>
+            <p className="text-[11px] text-gray-800 mt-2 font-light">Venice pavilion – Biennale The milk of dreams – Vincitore primo premio artefici del nostro tempo</p>
           </div>
         </div>
 
         <div className="mt-20">
           <h2 className="text-[10px] uppercase tracking-widest border-b pb-2 text-gray-700 font-light">Solo Shows</h2>
-          <div className="mt-5 text-[11px] text-gray-400 font-light">
+          <div className="mt-5 text-[11px] text-gray-700 font-light">
             <p>Afnakafna gallery 2019 – Moongloow – Roma</p>
             <p className="mt-2">Le nicchie gallery 2021 – Taranto</p>
           </div>
         </div>
 
         <div className="mt-24 border-t pt-10 text-[11px] text-gray-700 font-light">
-          <h2 className="text-[10px] uppercase tracking-widest text-gray-300 mb-4 font-light">Contact</h2>
+          <h2 className="text-[10px] uppercase tracking-widest text-gray-700 mb-4 font-light">Contact</h2>
           <p>Email: danieleraineri1990@gmail.com</p>
           <p className="mt-1">Instagram: @danieleraineristudio</p>
         </div>
