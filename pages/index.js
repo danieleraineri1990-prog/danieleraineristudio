70e01b4331d41935e9dc6628017be3c9097dd58c
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 13:00:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,17 +61,19 @@ export default function Home() {
           </div>
         </div>
               <div className="mb-40">
-  <img 
-    src="/images/33.JPG" 
-    alt="INTRECCIO NO.30" 
-    className="w-full h-auto" 
-  />
-  <div className="mt-6">
-    <p className="text-[12px] tracking-[0.1em] text-gray-800 font-normal italic">
-      INTRECCIO NO.30 — Oil 210x220
-    </p>
-  </div>
-</div>
+   <div className="mb-40">
+          <img 
+            src="/images/33.JPG" 
+            alt="INTRECCIO NO.30" 
+            className="w-full h-auto" 
+          />
+          <div className="mt-6">
+            <p className="text-[12px] tracking-[0.1em] text-gray-800 font-normal italic">
+              INTRECCIO NO.30 — Oil 210x220
+            </p>
+          </div>
+        </div>
+
 
 
         <div className="mt-12 text-[11px] leading-relaxed max-w-xl text-gray-700 font-light tracking-wide">
