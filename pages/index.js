1d4c28bd1deb285fb63acef61b551a5afb0369d3
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-07 16:51:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -299,9 +299,9 @@ export default function Home() {
         <div className="mt-20">
           <h2 className="text-[10px] uppercase tracking-widest border-b pb-2 text-gray-700 font-light">Solo Shows</h2>
           <div className="mt-5 text-[11px] text-gray-700 font-light">
-            <p>Afnakafna gallery 2019 – Moongloow – Roma</p>
-            <p>2025 collaborazione temporanea Artra gallery Milan</p>
-            <p className="mt-2">Le nicchie gallery 2021 – Taranto</p>
+            <p>2019Afnakafna gallery – Moongloow – Roma</p>
+            <p>2012 className="mt-2">Le nicchie gallery – Taranto</p>
+            <p>2025 Collaborazione temporanea Artra gallery Milan</p>
           </div>
         </div>
 
