3800419a4e08d9515e3a1c6cc76bc63d0923cf74
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-08 10:00:23 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -128,6 +128,13 @@ export default function Home() {
           </div>
         </div>
 
+          <div className="mt-20 mb-40">
+          <img src="/images/444.jpg" alt="TRACCIA NO.3" className="w-full h-auto" />
+          <div className="mt-4">
+            <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">TRACCIA NO.4 — Policarbonato/Carta 21x29</p>
+          </div>
+        </div>
+
         <div className="mb-40">
           <img src="/images/4.JPG" alt="INTRECCIO NO.5" className="w-full h-auto" />
           <div className="mt-4">
