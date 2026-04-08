4cfe1a1757c7c4803deba7c3d3a7562e8be704f5
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-08 19:47:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -72,11 +72,11 @@ export default function Home() {
           </div>
         </header>
 
- {/* Info fissa - Totalmente a destra e più in basso */}
+{/* Info fissa - Totalmente a destra e più in basso, font SPESSO ma SLANCIATO */}
 <div className="fixed bottom-4 right-0 z-50 pointer-events-none pr-0 text-right w-full">
   <span 
-    className="pointer-events-auto block w-full uppercase text-[16px] md:text-[20px] tracking-[0.2em] text-black"
-    style={{ fontWeight: '900' }} 
+    className="pointer-events-auto block w-full uppercase text-[16px] md:text-[20px] tracking-[0.1em] text-black"
+    style={{ fontWeight: '600', fontStretch: 'condensed', fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif' }}
   >
     SERIES: INTRECCI
   </span>
