73fcf7e875e169b7b5e00e9153213b0e168afaad
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-08 18:20:12 +0200

    Update disegni.js

--- pages/disegni.js
+++ pages/disegni.js
@@ -3,3 +3,15 @@
   alt="Daniele Raineri Studio" 
   className="w-full h-auto grayscale hover:grayscale-0 transition-all duration-700" 
 />
+
+    <img 
+  src="/images/2d.jpg" 
+  alt="Daniele Raineri Studio" 
+  className="w-full h-auto grayscale hover:grayscale-0 transition-all duration-700" 
+/>
+
+    <img 
+  src="/images/3d.jpg" 
+  alt="Daniele Raineri Studio" 
+  className="w-full h-auto grayscale hover:grayscale-0 transition-all duration-700" 
+/>
