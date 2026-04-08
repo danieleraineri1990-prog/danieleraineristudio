68b2b5dd9a002dbdd64fce76dc865f8ffe94b5bc
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-08 11:12:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -101,13 +101,20 @@ export default function Home() {
           </div>
         </div>
         
-         {/* INTRECCIO NO.1 - Ingrandito a tutto schermo su mobile */}
-          <div className="mt-40 mb-40">
-          <img src="/images/5.JPG" alt="INTRECCIO NO.1" className="w-full h-auto" />
-          <div className="mt-4">
-            <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">INTRECCIO NO.1 — Oil on linen 150x200</p>
-          </div>
-        </div>
+        
+        {/* INTRECCIO NO.1 - Ingrandito a tutto schermo su mobile */}
+<div className="mt-40 mb-40 -mx-10 md:mx-0">
+  <img 
+    src="/images/5.JPG" 
+    alt="INTRECCIO NO.1" 
+    className="w-full h-auto" 
+  />
+  <div className="mt-4 px-10 md:px-0">
+    <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light uppercase">
+      INTRECCIO NO.1 — Oil on linen 150x200
+    </p>
+  </div>
+</div>
           
           <div className="mt-20 mb-40">
           <img src="/images/222.jpg" alt="TRACCIA NO.2" className="w-full h-auto" />
