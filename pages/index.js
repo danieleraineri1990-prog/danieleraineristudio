c31d7d595954b665d33d1a30e108038042773e43
danieleraineri1990-prog <danieleraineri1990@gmail.com>
2026-04-08 10:20:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -229,7 +229,7 @@ export default function Home() {
         <div className="mt-20 mb-40">
           <img src="/images/21.JPG" alt="INTRECCIO NO.17" className="w-full h-auto" />
           <div className="mt-4">
-            <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">INTRECCIO NO.17 — Oil 80x120</p>
+            <p className="text-[11px] tracking-[0.1em] text-gray-400 font-light">INTRECCIO NO.17 — Acrylic 100x150</p>
           </div>
         </div>
 
